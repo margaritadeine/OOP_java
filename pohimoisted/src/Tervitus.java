368edee4ac0403773f2bb6ff1fed23a0ac91f7f2
margarita.deine <mademoselle@gmail.com>
2018-11-15 09:48:07 +0200

    Loodud Tervitus tüüpi konstruktor, mis võimaldab määrata tervitusõna Tervitus tüüpi objekti loomisel.

--- pohimoisted/src/Tervitus.java
+++ pohimoisted/src/Tervitus.java
@@ -1,12 +1,12 @@
 public class Tervitus {
-    public static String tervitusTekst;
-    public static void tervita() {
+    public String tervitusTekst;
+
+    public Tervitus(String t) {
+        this.tervitusTekst = t;
+    }
+
+    public void tervita() {
         System.out.println(tervitusTekst);
-
     }
 
-    public static void main(String[] args) {
-        tervitusTekst = "Tere Anna!";
-        tervita();
-    }
 }
---
+++ pohimoisted/src/TervitusTest.java
@@ -0,0 +1,6 @@
+public class TervitusTest {
+    public static void main(String[] args) {
+        Tervitus annaTervitus = new Tervitus("Tere õpilased!");
+        annaTervitus.tervita();
+    }
+}
