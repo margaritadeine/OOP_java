437646ca9c269c280897a21adaa339d8095a3f07
margarita.deine <mademoselle@gmail.com>
2018-11-28 09:39:15 +0200

    loodud seina kirjeldus

---
+++ pohimoisted/src/Sein.java
@@ -0,0 +1,14 @@
+public class Sein {
+    private String suund;
+
+
+    //konstruktor
+    public Sein(String suund) {
+        this.suund = suund;
+    }
+
+    //getter
+    public String getSuund() {
+        return suund;
+    }
+}
